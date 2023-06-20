e998e61ada82f01f98a03b3174b2cd95ece9c074
kubarada <jakub.rd@seznam.cz>
2023-06-20 15:27:47 +0200

    new methods + some examples

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import methods
 import datetime
 
 # downloading csv
-methods.download_data('https://onlinedata.plzen.eu/data-pd-rychtarka-actual.php', 'rychtarka_actual.csv')
-methods.download_data('https://onlinedata.plzen.eu/data-pd-novedivadlo-actual.php', 'nove_divadlo_actual.csv')
+#methods.download_data('https://onlinedata.plzen.eu/data-pd-rychtarka-actual.php', 'rychtarka_actual.csv')
+#methods.download_data('https://onlinedata.plzen.eu/data-pd-novedivadlo-actual.php', 'nove_divadlo_actual.csv')
+#methods.download_data('https://onlinedata.plzen.eu/data-pd-rychtarka.php', 'rychtarka_hist.csv')
+#methods.download_data('https://onlinedata.plzen.eu/data-pd-novedivadlo.php', 'nove_divadlo_hist.csv')
 
 # load data
 rychtarka_actual = methods.load_data('rychtarka_actual.csv')
 nove_divadlo_actual = methods.load_data('nove_divadlo_actual.csv')
+rychtarka_hist = methods.load_data('rychtarka_hist.csv')
+nove_divadlo_hist = methods.load_data('nove_divadlo_hist.csv')
 
-print('Volná místa Rychtářka: ', methods.available_places(rychtarka_actual)[0])
-print(methods.day_matrix(rychtarka_actual, 'pondělí'))
+print('Volná místa Nové divadlo: ', methods.available_places(nove_divadlo_actual)[0])
+print('Průměr ve středu na Rychtářce: ', methods.stats(methods.day_matrix(rychtarka_actual, 'úterý')))
+print('Obsazeno na Rychtářce: ', 100- methods.stats(methods.day_matrix(rychtarka_actual, 'úterý'))[2], '%')
+print('Průměr v 16:00:00 na Rychtářce: ', methods.stats(methods.hour_matrix(rychtarka_hist, '2:01:00')))
--- methods.py
+++ methods.py
@@ -1,7 +1,7 @@
 import numpy as np
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 
@@ -35,11 +35,26 @@ def day_matrix(csv_matrix, day):
             day_rows.append(row)
     return np.array(day_rows)
 
-def day_stats(day):
-    return 0
+def hour_matrix(csv_matrix, hour):
+    current = datetime.strptime(hour, '%H:%M:%S').time()
+    delta = timedelta(hours=0, minutes=10)
 
-def hour_stat(hour):
-    return 0
+    end_time = datetime.combine(datetime.today(), current) + delta
+    start_time = datetime.combine(datetime.today(), current) - delta
+    end_time = end_time.time()
+    start_time = start_time.time()
+
+    datetime_column = np.array([datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S').time() for date_str in csv_matrix[:, 7]])
+    filtered_matrix = csv_matrix[(datetime_column >= start_time) & (datetime_column <= end_time)]
+
+    return filtered_matrix
+
+def stats(filtered_matrix):
+    statistics = filtered_matrix[:, [1, 5]]
+    average_capacity = np.sum(statistics[:, 0]) / statistics.shape[0]
+    average_availability = np.sum(statistics[:, 1]) / statistics.shape[0]
+
+    return np.array([average_capacity, average_availability, average_availability/average_capacity*100]).astype(int)
 
 def day_hour_stat(day, hour):
     return 0
